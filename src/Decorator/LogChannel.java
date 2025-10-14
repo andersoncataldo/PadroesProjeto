878254f8f1a5920c689afb2d5198064590638016
Anderson Cataldo <andersonvcataldo@gmail.com>
2025-10-14 18:53:33 -0300

    new commit

--- src/Decorator/LogChannel.java
+++ src/Decorator/LogChannel.java
@@ -1,4 +1,20 @@
 package Decorator;
 
-public class LogChannel {
+public class LogChannel extends ChannelDecorator {
+
+    public LogChannel(Channel channel) {
+        super(channel);
+    }
+
+    @Override
+    public void send(String message) {
+        System.out.println("Registrando log de envio...");
+        super.send(message);
+    }
+
+    @Override
+    public String receive() {
+        System.out.println("Registrando log de recebimento...");
+        return super.receive();
+    }
 }
--- src/Decorator/Main.java
+++ src/Decorator/Main.java
@@ -1,4 +1,12 @@
 package Decorator;
 
 public class Main {
+    public static void main(String[] args) {
+        Channel tcp = new TCPChannel();
+
+        Channel secureChannel = new LogChannel(new ZipChannel(new BufferChannel(tcp)));
+
+        secureChannel.send("Olá, mundo!");
+        System.out.println(secureChannel.receive());
+    }
 }
